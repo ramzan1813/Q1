1413c2b4f0d317f56d9aabff6bd8a2cea1a24691
ramzan1813 <ramzanniazi85@outlook.com>
2021-06-22 10:30:48 +0500

    Q 1 Complete Now

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace Q1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a Number ");
+            int Number01 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter again a Number ");
+            int Number02 = Convert.ToInt32(Console.ReadLine());
+            int Numcber03=0;
+           
+           Console.WriteLine($" Before Swap Number 01 is : {Number01} ");
+
+           Console.WriteLine($" Before Swap Number 02 is : {Number02} ");
+            Numcber03 = Number01;
+            Number01 = Number02;
+            Number02 = Numcber03;
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine($" After Swap Number 01 is : {Number01} ");
+
+           Console.WriteLine($" After Swap Number 02 is : {Number02} ");
+
+           Console.ReadKey();
         }
     }
 }
